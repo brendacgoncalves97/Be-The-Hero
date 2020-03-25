2f528a927f1394e073cad4be685dfb6523037a14
Brenda Gonçalves <42783747+brendacgoncalves97@users.noreply.github.com>
2020-03-25 18:48:11 -0300

    Back-end da aplicação

---
+++ backend/src/controllers/SessionController.js
@@ -0,0 +1,15 @@
+const connection = require('../database/connection');
+
+module.exports = {
+    async index(request, response){
+        const { id } = request.body;
+
+        const ong = await  connection('ongs').where('id', id).select('name').first();
+
+        if (!ong){
+            return response.status(400).json({ error: 'No ONG found whith this ID' });
+        }
+
+        return response.json(ong);
+    }
+}
